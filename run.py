ef8498c0b9d3aba8cbc27c5bf406301765018311
周诚 <chengzcom0926@gmail.com>
2018-07-11 14:34:33 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,16 +1,2 @@
-import  subprocess
-
-
-try:
-    o = subprocess.check_output("pkill -f shadowsocks-server", shell=True)
-    print o
-except Exception as e:
-    print e
-    
-
-try:
-    o = subprocess.check_output("nohup /home/ubuntu/shadowsocks-server -p 443 -k applemac -m aes-128-cfb &", shell=True)
-    print o
-except Exception as e:
-    print e
-
+pkill -f shadowsocks-server
+nohup /home/ubuntu/shadowsocks-server -p 443 -k applemac -m aes-128-cfb &
